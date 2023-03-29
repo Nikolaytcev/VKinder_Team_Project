0d5c99e0fcfe26780517f0bfe2c1ed6fb8706e96
Nikolay1990 <Nikolaicev1990@gmail.com>
2023-03-29 21:36:13 +0300

    Проработано взаимодействие бота с БД

--- VK_interaction/bot.py
+++ VK_interaction/bot.py
@@ -15,33 +15,33 @@ class VKinderBot:
         self.vk_bot = vk_api.VkApi(token=bot_token)
         self.longpoll = VkLongPoll(self.vk_bot)
         self.user = {}
-        self.user_fields = ['country', 'city', 'bdate', 'sex']
-        self.user_candidates = []
+        self.user_fields = ["country", "city", "bdate", "sex"]
+        self.user_candidates = {}
         self.current_candidate = None
         self.users_data = {}
 
     def write_msg(self, message, keyboard=None, attachment=None):
         params = {
-            "user_id": self.user['id'],
+            "user_id": self.user["id"],
             "message": message,
-            "random_id": randrange(10 ** 7),
+            "random_id": randrange(10**7),
         }
         if keyboard:
-            if message == 'Пока((':
-                params['keyboard'] = keyboard.get_empty_keyboard()
+            if message == "Пока((":
+                params["keyboard"] = keyboard.get_empty_keyboard()
             else:
-                params['keyboard'] = keyboard.get_keyboard()
+                params["keyboard"] = keyboard.get_keyboard()
         if attachment:
-            params['attachment'] = attachment
+            params["attachment"] = attachment
 
         self.vk_bot.method("messages.send", params)
 
     def create_buttons(self):
         keyboard = VkKeyboard()
         buttons = [
-            text_emoji('В избранное', 'heart'),
-            text_emoji('Следующий', 'fast_forward'),
-            text_emoji('Не нравится', 'poop')
+            text_emoji("В избранное", "heart"),
+            text_emoji("Следующий", "fast_forward"),
+            text_emoji("Не нравится", "poop"),
         ]
         colors = [
             VkKeyboardColor.POSITIVE,
@@ -51,7 +51,7 @@ class VKinderBot:
         for btn, btn_color in zip(buttons, colors):
             keyboard.add_button(btn, btn_color)
         keyboard.add_line()
-        keyboard.add_button(text_emoji('Избранное', 'star'), VkKeyboardColor.PRIMARY)
+        keyboard.add_button(text_emoji("Избранное", "star"), VkKeyboardColor.PRIMARY)
         self.write_msg("Посмотри кого я нашел...", keyboard)
 
     def conversation(self):
@@ -59,20 +59,27 @@ class VKinderBot:
             if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
                 request = event.text
                 self.user = self.get_user_info(event.user_id)
-                if not self.user_candidates:
-                    self.user_candidates = self.vk_interaction.get_user_candidates(self.user)
+
+                self.user_candidates[event.user_id] = self.vk_interaction.get_user_candidates(self.user)
+
                 request_list = {
                     "привет": {
                         self.write_msg: [f"Привет, {self.user['first_name']}!"],
                         self.create_buttons: [],
                         self.next_user: [],
-                        self.add_user_db: [request]
+                        self.add_user_db: [request],
                     },
                     "пока": {self.write_msg: ["Пока((", VkKeyboard()]},
-                    text_emoji('Следующий', 'fast_forward'): {self.next_user: []},
-                    text_emoji('В избранное', 'heart'): {self.add_user_db: [request], self.add_relation: ['Favorite']},
-                    text_emoji('Избранное', 'star'): {self.show_like_list: []},
-                    text_emoji('Не нравится', 'poop'): {self.add_user_db: [request], self.add_relation: ['Blacklist']},
+                    text_emoji("Следующий", "fast_forward"): {self.next_user: []},
+                    text_emoji("В избранное", "heart"): {
+                        self.add_user_db: [request],
+                        self.add_relation: ["Favorite"],
+                    },
+                    text_emoji("Избранное", "star"): {self.show_like_list: []},
+                    text_emoji("Не нравится", "poop"): {
+                        self.add_user_db: [request],
+                        self.add_relation: ["Blacklist"],
+                    },
                 }
                 if request_list.get(request) is None:
                     self.write_msg("А можно поподробнее?...")
@@ -81,85 +88,123 @@ class VKinderBot:
                         k(*val)
 
     def add_user_db(self, request):
-        sex = {2: 'male', 1: 'female'}
-        if request == 'привет':
-            if not self.db.check_user_in_db(self.user['id']).all():
-                self.db.add_user([self.user['id'], self.user['first_name'],
-                                  self.user['last_name'], sex.get(self.user['sex']),
-                                  self.user['city']['title']])
+        sex = {2: "male", 1: "female"}
+        if request == "привет":
+            if not self.db.check_user_in_db(self.user["id"]).all():
+                self.db.add_user(
+                    [
+                        self.user["id"],
+                        self.user["first_name"],
+                        self.user["last_name"],
+                        sex.get(self.user["sex"]),
+                        self.user["city"]["title"],
+                    ]
+                )
         else:
-            if not self.db.check_user_in_db(self.users_data.get(self.user['id'])).all():
-                like_person = self.get_user_info(self.users_data.get(self.user['id']))
-                self.db.add_user([like_person['id'], like_person['first_name'],
-                                  like_person['last_name'], sex.get(like_person['sex']),
-                                  like_person['city']['title']])
+            if not self.db.check_user_in_db(self.users_data.get(self.user["id"])).all():
+                like_person = self.get_user_info(self.users_data.get(self.user["id"]))
+                self.db.add_user(
+                    [
+                        like_person["id"],
+                        like_person["first_name"],
+                        like_person["last_name"],
+                        sex.get(like_person["sex"]),
+                        like_person["city"]["title"],
+                    ]
+                )
 
     def next_user(self):
-        self.current_candidate = self.user_candidates.pop()
+
+        self.current_candidate = self.user_candidates.get(self.user['id']).pop()
+
         try:
-            favorite_list = [i[0] for i in self.db.get_blocked_list(self.user['id'])]
-            blocked_list = [i[0] for i in self.db.get_favorite_list(self.user['id'])]
-            if self.current_candidate['id'] not in favorite_list and self.current_candidate['id'] not in blocked_list:
-                self.users_data[self.user['id']] = self.current_candidate["id"]
+            favorite_list = [i[0] for i in self.db.get_blocked_list(self.user["id"])]
+            blocked_list = [i[0] for i in self.db.get_favorite_list(self.user["id"])]
+            if (
+                self.current_candidate["id"] not in favorite_list
+                and self.current_candidate["id"] not in blocked_list
+            ):
+                self.users_data[self.user["id"]] = self.current_candidate["id"]
                 user_name = f"{self.current_candidate['first_name']} {self.current_candidate['last_name']}"
                 link = f'https://vk.com/id{self.current_candidate["id"]}'
-                photos = self.vk_interaction.photos_get(self.current_candidate['id'])
-                message = f'{user_name}\r\n{link}'
+                photos = self.vk_interaction.photos_get(self.current_candidate["id"])
+                message = f"{user_name}\r\n{link}"
                 attachments = []
                 i = 0
                 for photo in photos:
                     if i > 2:
                         break
                     i += 1
-                    attachments.append(f'photo{self.current_candidate["id"]}_{photo["id"]}')
-                attachment = ','.join(attachments)
+                    attachments.append(
+                        f'photo{self.current_candidate["id"]}_{photo["id"]}'
+                    )
+                attachment = ",".join(attachments)
                 self.write_msg(message, attachment=attachment)
             else:
                 self.next_user()
         except vk_api.exceptions.ApiError:
-            print('This profile is private')
+            print("This profile is private")
             self.next_user()
 
     def __check_mutual_sympathy(self):
-        user_favorite_list = [i[0] for i in self.db.get_favorite_list(self.user['id'])]
-        candidate_favorite_list = [i[0] for i in self.db.get_favorite_list(self.users_data.get(self.user['id']))]
-        return self.user['id'] in candidate_favorite_list and self.users_data.get(self.user['id']) in user_favorite_list
+        user_favorite_list = [i[0] for i in self.db.get_favorite_list(self.user["id"])]
+        candidate_favorite_list = [
+            i[0]
+            for i in self.db.get_favorite_list(self.users_data.get(self.user["id"]))
+        ]
+        return (
+            self.user["id"] in candidate_favorite_list
+            and self.users_data.get(self.user["id"]) in user_favorite_list
+        )
 
     def __get_message_info(self, user):
-        name = f'{user[1]} {user[2]}'
-        link = f'https://vk.com/id{user[0]}'
+        name = f"{user[1]} {user[2]}"
+        link = f"https://vk.com/id{user[0]}"
         photos = self.vk_interaction.photos_get(user[0])
-        attachment = ','.join([f'photo{user[0]}_{ph["id"]}' for idx, ph in enumerate(photos) if idx < 4])
-        message = f'{name}\r\n{link}'
+        attachment = ",".join(
+            [f'photo{user[0]}_{ph["id"]}' for idx, ph in enumerate(photos) if idx < 4]
+        )
+        message = f"{name}\r\n{link}"
         return attachment, message
 
     def add_relation(self, status):
-        self.db.add_relation(self.user['id'], self.users_data.get(self.user['id']), status=status)
+        self.db.add_relation(
+            self.user["id"], self.users_data.get(self.user["id"]), status=status
+        )
         if self.__check_mutual_sympathy():
-            attachment, message = self.__get_message_info(self.get_user_info(self.users_data.get(self.user['id'])))
-            self.write_msg(f' У вас взаимная симпатия с {message}', attachment=attachment)
+            attachment, message = self.__get_message_info(
+                self.get_user_info(self.users_data.get(self.user["id"]))
+            )
+            self.write_msg(
+                f" У вас взаимная симпатия с {message}", attachment=attachment
+            )
         else:
             self.next_user()
 
     def show_like_list(self):
-        like_list = self.db.get_favorite_list(self.user['id'])
-        self.write_msg('Список избранных: ')
+        like_list = self.db.get_favorite_list(self.user["id"])
+        self.write_msg("Список избранных: ")
         if like_list:
             for i in like_list:
                 attachment, message = self.__get_message_info(i)
                 self.write_msg(message, attachment=attachment)
 
     def get_user_info(self, user_id):
-        values = {
-            'user_ids': user_id,
-            'fields': ', '.join(self.user_fields)
-        }
-        user_info = self.vk_bot.method('users.get', values=values)
+        values = {"user_ids": user_id, "fields": ", ".join(self.user_fields)}
+        user_info = self.vk_bot.method("users.get", values=values)
         return user_info[0]
 
 
 def text_emoji(text, emoji_alias):
-    return emoji.emojize(f'{text} :{emoji_alias}:', language='alias')
+    return emoji.emojize(f"{text} :{emoji_alias}:", language="alias")
 
 
+def main():
+    db = connect_to_db('vkinder1_db')
+    token = 'vk1.a.3hLJWip_fL-k0tgKoUg-Kn0EUKjboWC8GD8dQl_As0aVWMVen7E3SfaaXpQXpifxHwTUVrtFnmNH891_CpZNuzSp89VXUYyMhRPrkGxibgKaR5778xzpos0-NQETjqVMfThGKSeMtCIFi8Nk4MaqDUaeN5zHoQOgKuP-rDy2UXTtEwLYrQJNPDzWoLWYXB6J-wQ5fTFk4OEu-mdITGWPgw'
+    token_vk = 'vk1.a.Rm4LAwHRhZlM38N8Vr8j5X6HLykqNbNXWz31PHeksn-JY8SZctMDLrZiLa2M0N0u5_xRHsY48YH-6UxAYZ7evrIvekjlQUXop2POqdEt_ORD3zCFrA5m63qs4__PXxrvxa455rNVQmSy6VTuk55_z8AuU1wHhmFzqossMDJvtRij5US3XD1iu-g2j6Ryuj0sMJMzyKdLCYB1lpFXMt1Pxw'
+    VKinderBot(token, token_vk, db).conversation()
 
+
+if __name__ == "__main__":
+    main()
